fa26213516074377c830f93ea781e45ea3c9132b
Ary <ketutarysujaya@gmail.com>
2026-08-27 12:11:00 +0800

    Update soal-tiu.js

--- soal-tiu.js
+++ soal-tiu.js
@@ -1,1 +1,387 @@
-
+const soalTIU = [
+  {
+    soal: "Pancasila : Adab = Integritas : ...",
+    opsi: [
+      "Peduli",
+      "Tangguh",
+      "Kerja Keras",
+      "Sederhana",
+      "Berani"
+    ],
+    kunci: 1
+  },
+  {
+    soal: "Harimau : ... : Reptil = Merak : Taman Safari : ...",
+    opsi: [
+      "Kebun ; Mamalia",
+      "Jalan ; Burung",
+      "Sawah ; Aves",
+      "Taman Margasatwa : Burung",
+      "Bonbin ; Ikan"
+    ],
+    kunci: 4
+  },
+  {
+    soal: "Bapak membakar lilin, mengenakan baju, dan mengetim kentang.<br><br>Kalimat di atas memiliki hubungan yang sama dengan kalimat ....",
+    opsi: [
+      "Kakak menyulut petasan, memasang kamera, dan memanggang daging",
+      "Adik membakar api unggun, membangun tenda, dan membakar ikan",
+      "Bibi menyalakan lampu, memakai jaket, dan mengukus ubi",
+      "Kakek menghidupkan motor, mengenakan helm, dan memanaskannya",
+      "Paman mengaktifkan genset, memakai masker, dan menanak nasi"
+    ],
+    kunci: 2
+  },
+  {
+    soal: "Seorang mahasiswa X harus bekerja tambahan setelah pulang kuliah. Pekerjaan tambahan tersebut menyebabkan waktu belajarnya berkurang. Meskipun X hanya bekerja di waktu malam, perkuliahannya tetap terganggu karena waktu belajarnya berkurang.<br><br>Berdasarkan informasi di atas, manakah pernyataan di bawah ini yang benar?",
+    opsi: [
+      "X bekerja pada malam hari karena perkuliahannya terganggu",
+      "Pekerjaan tambahan menyebabkan perkuliahan X terganggu",
+      "Jam kerja di malam hari menyebabkan X mencari pekerjaan tambahan",
+      "X mencari pekerjaan tambahan ketika jam belajarnya berkurang",
+      "Waktu belajar X berkurang ketika perkuliahannya terganggu"
+    ],
+    kunci: 1
+  },
+  {
+    soal: "Banyak petani menggunakan jasa buruh sehingga pengeluaran membengkak. Pembengkakan pengeluaran meningkatkan modal yang harus disiapkan petani pada musim tanam padi. Meskipun sawah dapat ditanami palawija, peminjaman modal pada koperasi tidak dapat dihindari karena modal yang harus disiapkan petani meningkat.<br><br>Berdasarkan informasi di atas, manakah pernyataan di bawah ini yang benar?",
+    opsi: [
+      "Penanaman palawija di sawah menyebabkan peningkatan modal yang harus disiapkan petani pada musim tanam padi",
+      "Petani menggunakan jasa buruh yang disebabkan oleh peminjaman modal di koperasi yang tidak dapat dihindari",
+      "Penggunaan jasa buruh menyebabkan peningkatan modal petani pada musim tanam padi",
+      "Sawah ditanami palawija hanya pada musim tanam padi",
+      "Petani menggunakan jasa buruh karena sawah bisa ditanami palawija"
+    ],
+    kunci: 2
+  },
+  {
+    soal: "Perluasan tanah untuk ekspansi perkebunan kelapa sawit menyebabkan penurunan keanekaragaman hayati. Pengurangan perluasan tanah untuk ekspansi perkebunan kelapa sawit adalah dengan melakukan intensifikasi lahan perkebunan kelapa sawit yang sudah ada. Risiko penurunan keanekaragaman hayati akan berkurang jika perluasan tanah untuk ekspansi perkebunan kelapa sawit dapat dicegah.<br><br>Berdasarkan informasi tersebut, manakah pernyataan berikut yang pasti benar?",
+    opsi: [
+      "Perluasan tanah untuk ekspansi perkebunan sawit dapat diatasi dengan meningkatkan keanekaragaman hayati",
+      "Perluasan tanah untuk ekspansi perkebunan sawit tidak dapat dilakukan dengan intensifikasi lahan perkebunan kelapa sawit yang sudah ada",
+      "Meningkatkannya keanekaragaman hayati dapat dilakukan dengan intensifikasi lahan perkebunan sawit yang sudah ada",
+      "Tidak adanya program intensifikasi lahan perkebunan sawit menyebabkan tingginya keanekaragaman hayati",
+      "Intensifikasi lahan perkebunan sawit yang sudah ada menurunkan keanekaragaman hayati"
+    ],
+    kunci: 2
+  },
+  {
+    soal: "Suatu tim akan melaksanakan Sensus Ekonomi dengan mengunjungi enam rumah, yakni Pande, Qori, Rendy, Sonia, Toti, dan Ulul secara bergantian.<br>- Rumah Sonia hanya dapat dikunjungi jika sudah mengunjungi rumah Rendy dan Toti.<br>- Tim tersebut tidak dapat mengunjungi rumah Toti sebelum mengunjungi rumah Pande.<br>- Rumah kedua yang dikunjungi adalah rumah Qori.<br><br>Pernyataan yang benar adalah ...",
+    opsi: [
+      "Rumah Toti pada urutan keenam",
+      "Rumah Sonia pada urutan keempat",
+      "Rumah Pande pada urutan kesatu",
+      "Rumah Rendy pada urutan keenam",
+      "Rumah Toti pada urutan pertama"
+    ],
+    kunci: 2
+  },
+  {
+    soal: "Lula, Albert, dan Susi berada di sebuah toko. Mereka sepakat untuk membeli jaket dengan warna yang sudah ditentukan, yaitu kuning, biru, hijau, dan merah dengan aturan sebagai berikut.<br>- Lula tidak membeli jaket warna kuning.<br>- Jika dan hanya jika ada yang membeli jaket warna biru, maka tidak ada yang memilih warna hijau.<br>- Jika Albert membeli jaket warna merah, maka Susi memilih warna hijau.<br><br>Jika Lula membeli jaket warna hijau dan Albert membeli jaket warna kuning, maka pernyataan yang benar adalah ….",
+    opsi: [
+      "Lula juga membeli jaket warna biru",
+      "Albert dan Susi sama-sama membeli jaket warna merah",
+      "Lula dan Susi bisa membeli jaket warna hijau",
+      "Hanya Albert yang bisa membeli jaket warna kuning",
+      "Susi bisa membeli jaket warna kuning atau biru"
+    ],
+    kunci: 2
+  },
+  {
+    soal: "Xavier menyusun rencana kursus tiga dari lima mata pelajaran yang bisa diambil, yaitu matematika, bahasa Inggris, IPS, IPA, dan kesenian. Peraturan mengambil kursus sebagai berikut.<br>- Xavier harus mengambil tiga mata pelajaran.<br>- Jika mengambil kesenian, ia tidak boleh mengambil bahasa Inggris.<br>- Jika mengambil IPA, ia harus mengambil matematika, demikian pula sebaliknya.<br>- Jika mengambil kesenian, ia dapat mengambil IPS.<br><br>Pasangan mata pelajaran yang tidak bisa diambil di kursus tersebut secara bersamaan adalah ….",
+    opsi: [
+      "IPS dan bahasa Inggris",
+      "IPA dan matematika",
+      "Matematika dan IPS",
+      "IPA dan IPS",
+      "Kesenian dan matematika"
+    ],
+    kunci: 4
+  },
+  {
+    soal: "Sebuah kompleks terdiri dari 3 buah rumah, yaitu rumah Arna, Ben, dan Chen. Rumah-rumah tersebut pada hari Jumat, Sabtu, dan Minggu selalu memesan makanan yang berbeda untuk hari-hari tersebut pada sebuah restoran. Setiap rumah selalu memesan menu yang berbeda dan setiap menu hanya boleh dipesan satu kali oleh setiap rumah. Terdapat 4 menu pada restoran tersebut, yaitu bakso, mie, sate, dan tongseng. Rumah Arna dan Chen tidak pernah memesan sate. Tiga rumah suka dengan mie dan bakso. Suatu ketika, rumah Ben memesan sate pada hari Sabtu dan tidak memesan tongseng pada hari setelahnya ataupun sebelumnya. Rumah Arna memesan mie pada hari Minggu. Apa yang dipesan rumah Arna pada hari Jumat dan Minggu?",
+    opsi: [
+      "Sate dan tongseng",
+      "Mie dan sate",
+      "Bakso dan mie",
+      "Bakso dan sate",
+      "Bakso dan tongseng"
+    ],
+    kunci: 2
+  },
+  {
+    soal: "Hasil dari (3/2 + 7/3) × 3/5 – 11/10 = ...",
+    opsi: [
+      "4/5",
+      "9/5",
+      "8/5",
+      "7/5",
+      "6/5"
+    ],
+    kunci: 4
+  },
+  {
+    soal: "Hasil dari 1/5 × 8/3 – 6/5 + 4/3 = ...",
+    opsi: [
+      "2/3",
+      "4/3",
+      "3/5",
+      "1/6",
+      "3/7"
+    ],
+    kunci: 0
+  },
+  {
+    soal: "Hasil dari 1/4 + 3/5 × 5/8 – 1/2 × 1/2 = ...",
+    opsi: [
+      "1/8",
+      "1/4",
+      "1/3",
+      "3/8",
+      "3/4"
+    ],
+    kunci: 3
+  },
+  {
+    soal: "Hasil dari 6/15 – (–6) : 7 2/4 × (–2) + 1/5 – 2,4 = ...",
+    opsi: [
+      "0",
+      "–1",
+      "–1,4",
+      "–3,4",
+      "–2"
+    ],
+    kunci: 3
+  },
+  {
+    soal: "100%; 2 2/3; 0,75; 66 2/3 %; 1/2; –4/3; ... ; ...",
+    opsi: [
+      "12 1/2 %; 1/2",
+      "1/4; 2 1/3",
+      "0,5; –8/3",
+      "2/8, –3 1/3",
+      "0,33...; –1/2"
+    ],
+    kunci: 3
+  },
+  {
+    soal: "5, 8, 7, –14, –11, –12, 24, 27, …",
+    opsi: [
+      "25",
+      "26",
+      "28",
+      "–15",
+      "–17"
+    ],
+    kunci: 1
+  },
+  {
+    soal: "1, 9, 49, 169, …, 961",
+    opsi: [
+      "649",
+      "629",
+      "549",
+      "529",
+      "441"
+    ],
+    kunci: 4
+  },
+  {
+    soal: "2, –1, –2, 2, –4, –8, 32, …",
+    opsi: [
+      "–32",
+      "64",
+      "–128",
+      "128",
+      "–256"
+    ],
+    kunci: 4
+  },
+  {
+    soal: "Diketahui<br><br><img src='gambar/no49_soal_paket0.png' style='max-width: 100%; height: auto;'><br><br>Manakah hubungan yang benar antara kuantitas M dan M berikut berdasarkan informasi yang diberikan?",
+    opsi: [
+      "<img src='gambar/no49_opsiA_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no49_opsiB_paket0.png' style='height: 80px; width: auto;'>",
+      "Hubungan M dan N tidak dapat ditentukan",
+      "<img src='gambar/no49_opsiD_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no49_opsiE_paket0.png' style='height: 80px; width: auto;'>"
+    ],
+    kunci: 3
+  },
+  {
+    soal: "Diketahui<br><br><img src='gambar/no50_soal_paket0.png' style='max-width: 100%; height: auto;'><br><br>Manakah hubungan yang benar antara kuantitas X dan Y berikut berdasarkan informasi yang diberikan?",
+    opsi: [
+      "<img src='gambar/no50_opsiA_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no50_opsiB_paket0.png' style='height: 80px; width: auto;'>",
+      "Hubungan X dan Y tidak dapat ditentukan",
+      "<img src='gambar/no50_opsiD_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no50_opsiE_paket0.png' style='height: 80px; width: auto;'>"
+    ],
+    kunci: 4
+  },
+  {
+    soal: "Bus pertama berangkat dari kota A pukul 07.35 WIB dengan kecepatan 80 km/jam dan tiba di kota B pukul 10.05 WIB. Manakah hubungan yang benar antara kuantitas P dan Q berikut berdasarkan informasi yang diberikan?<br><br><img src='gambar/no51_soal_paket0.png' style='max-width: 100%; height: auto;'>",
+    opsi: [
+      "<img src='gambar/no51_opsiA_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no51_opsiB_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no51_opsiC_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no51_opsiD_paket0.png' style='height: 80px; width: auto;'>",
+      "Informasi yang diberikan tidak cukup untuk memutuskan salah satu dari empat pilihan di atas"
+    ],
+    kunci: 2
+  },
+  {
+    soal: "Diketahui nilai x = 2 dan y = 4.<br><br><img src='gambar/no52_soal_paket0.png' style='max-width: 100%; height: auto;'><br><br>Manakah hubungan yang benar antara kuantitas A dan B berikut berdasarkan informasi yang diberikan?",
+    opsi: [
+      "<img src='gambar/no52_opsiA_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no52_opsiB_paket0.png' style='height: 80px; width: auto;'>",
+      "Hubungan A dan B tidak dapat ditentukan",
+      "<img src='gambar/no52_opsiD_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no52_opsiE_paket0.png' style='height: 80px; width: auto;'>"
+    ],
+    kunci: 1
+  },
+  {
+    soal: "Dalam waktu 6 hari, 15 orang pegawai konveksi dapat membuat 100 lusin celana. Untuk membuat 800 celana dalam waktu 10 hari, pengurangan pegawai yang dilakukan sebanyak … orang.",
+    opsi: [
+      "12",
+      "9",
+      "6",
+      "3",
+      "2"
+    ],
+    kunci: 1
+  },
+  {
+    soal: "Mesin A dapat membuat 600 baut tiap 12 menit, sedangkan mesin B dapat membuat 450 baut tiap 15 menit. Tentukan berapa menit waktu yang diperlukan oleh kedua mesin untuk membuat 2800 baut?",
+    opsi: [
+      "45 menit",
+      "30 menit",
+      "35 menit",
+      "50 menit",
+      "55 menit"
+    ],
+    kunci: 2
+  },
+  {
+    soal: "Seorang pelari dapat menyelesaikan lari sejauh 5 kilometer dalam waktu 30 menit. Jika dia melatih dirinya dengan meningkatkan kecepatannya dan dia dapat melakukan suatu latihan lari dalam waktu 75 menit, berapa jarak yang ditempuhnya?",
+    opsi: [
+      "10 kilometer",
+      "12,5 kilometer",
+      "13 kilometer",
+      "14,5 kilometer",
+      "15 kilometer"
+    ],
+    kunci: 1
+  },
+  {
+    soal: "<img src='gambar/no56_soal_paket0.png' style='max-width: 400px; width: 100%; height: auto;'>",
+    opsi: [
+      "<img src='gambar/no56_opsiA_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no56_opsiB_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no56_opsiC_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no56_opsiD_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no56_opsiE_paket0.png' style='height: 80px; width: auto;'>"
+    ],
+    kunci: 3
+  },
+  {
+    soal: "<img src='gambar/no57_soal_paket0.png' style='max-width: 400px; width: 100%; height: auto;'>",
+    opsi: [
+      "<img src='gambar/no57_opsiA_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no57_opsiB_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no57_opsiC_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no57_opsiD_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no57_opsiE_paket0.png' style='height: 80px; width: auto;'>"
+    ],
+    kunci: 1
+  },
+  {
+    soal: "<img src='gambar/no58_soal_paket0.png' style='max-width: 400px; width: 100%; height: auto;'>",
+    opsi: [
+      "<img src='gambar/no58_opsiA_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no58_opsiB_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no58_opsiC_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no58_opsiD_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no58_opsiE_paket0.png' style='height: 80px; width: auto;'>"
+    ],
+    kunci: 1
+  },
+  {
+    soal: "<img src='gambar/no59_soal_paket0.png' style='max-width: 400px; width: 100%; height: auto;'>",
+    opsi: [
+      "<img src='gambar/no59_opsiA_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no59_opsiB_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no59_opsiC_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no59_opsiD_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no59_opsiE_paket0.png' style='height: 80px; width: auto;'>"
+    ],
+    kunci: 3
+  },
+  {
+    soal: "<img src='gambar/no60_soal_paket0.png' style='max-width: 400px; width: 100%; height: auto;'>",
+    opsi: [
+      "<img src='gambar/no60_opsiA_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no60_opsiB_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no60_opsiC_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no60_opsiD_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no60_opsiE_paket0.png' style='height: 80px; width: auto;'>"
+    ],
+    kunci: 4
+  },
+  {
+    soal: "<img src='gambar/no61_soal_paket0.png' style='max-width: 400px; width: 100%; height: auto;'>",
+    opsi: [
+      "<img src='gambar/no61_opsiA_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no61_opsiB_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no61_opsiC_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no61_opsiD_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no61_opsiE_paket0.png' style='height: 80px; width: auto;'>"
+    ],
+    kunci: 3
+  },
+  {
+    soal: "<img src='gambar/no62_soal_paket0.png' style='max-width: 400px; width: 100%; height: auto;'>",
+    opsi: [
+      "<img src='gambar/no62_opsiA_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no62_opsiB_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no62_opsiC_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no62_opsiD_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no62_opsiE_paket0.png' style='height: 80px; width: auto;'>"
+    ],
+    kunci: 4
+  },
+  {
+    soal: "<img src='gambar/no63_soal_paket0.png' style='max-width: 400px; width: 100%; height: auto;'>",
+    opsi: [
+      "<img src='gambar/no63_opsiA_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no63_opsiB_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no63_opsiC_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no63_opsiD_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no63_opsiE_paket0.png' style='height: 80px; width: auto;'>"
+    ],
+    kunci: 4
+  },
+  {
+    soal: "<img src='gambar/no64_soal_paket0.png' style='max-width: 400px; width: 100%; height: auto;'>",
+    opsi: [
+      "<img src='gambar/no64_opsiA_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no64_opsiB_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no64_opsiC_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no64_opsiD_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no64_opsiE_paket0.png' style='height: 80px; width: auto;'>"
+    ],
+    kunci: 3
+  },
+  {
+    soal: "<img src='gambar/no65_soal_paket0.png' style='max-width: 400px; width: 100%; height: auto;'>",
+    opsi: [
+      "<img src='gambar/no65_opsiA_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no65_opsiB_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no65_opsiC_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no65_opsiD_paket0.png' style='height: 80px; width: auto;'>",
+      "<img src='gambar/no65_opsiE_paket0.png' style='height: 80px; width: auto;'>"
+    ],
+    kunci: 2
+  }
+];
