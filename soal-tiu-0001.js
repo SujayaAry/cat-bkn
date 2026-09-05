b14996a7f64c1d7bcb45320487e352e7a406e9f0
Ary <ketutarysujaya@gmail.com>
2026-09-05 11:15:31 +0800

    Create soal-tiu-0001.js

---
+++ soal-tiu-0001.js
@@ -0,0 +1,387 @@
+const soalTIU_0001 = [
+  {
+    soal: "Berlian : Karat = …",
+    opsi: [
+      "Ustaz : Doa",
+      "Buku : Dungu",
+      "Bensin : Galon",
+      "Besi : Karat",
+      "Kayu : Perajin"
+    ],
+    kunci: 2
+  },
+  {
+    soal: "Hubungan objek pada kalimat “Dokter itu membawa stetoskop dan suntik saat berada di rumah sakit” setara dengan ….",
+    opsi: [
+      "Guru itu membawa spidol dan buku saat terlihat di jalan",
+      "Mahasiswa itu mengenakan selempang dan undangan saat wisuda",
+      "Siswa itu mengenakan seragam dan tas di sekolahnya",
+      "Pemadam kebakaran membawa topi dan tabung oksigen ke TKP",
+      "Pedagang itu membawa kalkulator dan asisten ke rumah pelanggannya"
+    ],
+    kunci: 1
+  },
+  {
+    soal: "Pelanggan itu mendapatkan rabat saat membeli buku antologi elegi yang sarat akan pesan afeksi.<br><br>Pola hubungan makna pada kalimat tersebut ekuivalen dengan kalimat ....",
+    opsi: [
+      "Pelaku itu merasa jera setelah mencuri buku antologi puisi cinta yang sarat akan luapan emosi",
+      "Pembeli itu mendapatkan bonus daging saat memborong buku antologi puisi religi yang sarat akan pesan cinta",
+      "Pedagang itu mengambil keuntungan saat menjual buku antologi prosa kehidupan yang sarat akan rasa jengkel",
+      "Karyawan itu menuntut tambahan gaji setelah menyusun buku antologi prosa kematian yang sarat akan nada marah",
+      "Pelanggan itu mendapatkan potongan saat membeli buku antologi puisi sedih yang sarat akan pesan kasih sayang"
+    ],
+    kunci: 4
+  },
+  {
+    soal: "Karakteristik anatomi pasti dari tanaman berbiji keping dua teridentifikasi dari sistem perakarannya yang berjenis tunggang. Adapun famili botani dari kelompok tumbuhan palem secara mutlak mengembangkan struktur akar serabut. Saat dilakukan observasi di lapangan, terkonfirmasi bahwa spesimen tanaman X masuk ke dalam klasifikasi berbiji keping dua.<br><br>Berdasarkan informasi tersebut, manakah simpulan yang pasti benar?",
+    opsi: [
+      "Spesimen tanaman X dipastikan tidak termasuk dalam famili tumbuhan palem.",
+      "Satu-satunya famili tumbuhan palem yang mengembangkan akar tunggang hanyalah spesimen tanaman X.",
+      "Spesimen tanaman X teridentifikasi memiliki struktur perakaran serabut.",
+      "Spesimen tanaman X merupakan famili tumbuhan palem yang memiliki akar tunggang.",
+      "Spesimen tanaman X diklasifikasikan sebagai jenis dari famili tumbuhan palem."
+    ],
+    kunci: 0
+  },
+  {
+    soal: "Kompetensi berbahasa Inggris merupakan atribut pasti yang melekat pada identitas peserta didik. Penguasaan kompetensi ini secara mutlak menumbuhkan kegemaran yang tinggi untuk berdiskusi. Namun, besarnya kegemaran berdiskusi tersebut justru selalu diiringi dengan ketidakmampuan mereka saat harus berdebat.<br><br>Berdasarkan informasi tersebut, simpulan mana yang pasti benar?",
+    opsi: [
+      "Peserta didik yang menguasai kompetensi berbahasa Inggris dipastikan tidak memiliki kecakapan berdebat.",
+      "Peserta didik yang menjauhi kegiatan diskusi dipastikan berstatus mahir berbahasa Inggris.",
+      "Peserta didik dengan kompetensi berbahasa Inggris terbukti tidak memiliki minat untuk berdiskusi.",
+      "Peserta didik yang gemar berdiskusi dipastikan buta bahasa Inggris dan tidak cakap berdebat.",
+      "Peserta didik yang kehilangan kecakapan berdebat secara otomatis berstatus mahir berbahasa Inggris."
+    ],
+    kunci: 0
+  },
+  {
+    soal: "Keanggotaan ekstrakurikuler kesenian secara mutlak mewajibkan siswanya untuk turut serta dalam klub bela diri ataupun renang. Sementara itu, seluruh anggota dari kedua klub olahraga tersebut dipastikan juga terdaftar aktif pada kelas melukis. Saat dievaluasi, Inda terbukti tidak memiliki riwayat pendaftaran pada kelas melukis tersebut.<br><br>Berdasarkan informasi di atas, manakah simpulan yang pasti benar?",
+    opsi: [
+      "Inda merupakan anggota aktif klub bela diri.",
+      "Inda berstatus sebagai anggota ekstrakurikuler kesenian.",
+      "Inda dipastikan tidak tergabung dalam ekstrakurikuler kesenian.",
+      "Inda adalah anggota klub renang yang menolak bergabung di kelas melukis.",
+      "Inda absen dari ekstrakurikuler kesenian, namun terdaftar pada klub bela diri."
+    ],
+    kunci: 2
+  },
+  {
+    soal: "Sebuah kelompok siswa terdiri dari 6 orang siswa sedang mengantre, yaitu Al, Bam, Carl, Dini, Erik, dan Ferdi. Carl selalu ingin dekat dengan Bam, tetapi tidak ingin dekat dengan Dini. Ferdi selalu ingin dekat dengan Bam dan Dini. Al selalu ingin berada di paling depan. Kemungkinan urutannya dari paling depan adalah ….",
+    opsi: [
+      "Al, Bam, Carl, Dini, Erik, Ferdi",
+      "Al, Bam, Carl, Ferdi, Dini, Erik",
+      "Al, Bam, Carl, Ferdi, Erik, Dini",
+      "Al, Carl, Bam, Dini, Ferdi, Erik",
+      "Al, Carl, Bam, Ferdi, Dini, Erik"
+    ],
+    kunci: 4
+  },
+  {
+    soal: "Menjelang Tahun Baru, sebuah pusat perbelanjaan menawarkan diskon besar-besaran. Banyak pengunjung berburu barang-barang mewah di malam pergantian tahun. Ketika didata, pengunjung yang masuk ke dalam pusat perbelanjaan tersebut mempunyai urutan sebagai berikut.<br>- Endra memasuki pusat perbelanjaan tiga menit sebelum Niki.<br>- Niki mulai berbelanja tujuh menit setelah Bima.<br>- Bima berbelanja jaket dan celana denim.<br>- Niki keluar dari pusat perbelanjaan dua menit sebelum Bima.<br>- Bima selesai berbelanja empat menit setelah Endra.<br><br>Bagaimanakah urutan pengunjung dari yang tercepat menyelesaikan pembelian?",
+    opsi: [
+      "Endra – Bima – Niki",
+      "Bima – Endra – Niki",
+      "Niki – Endra – Bima",
+      "Endra – Niki – Bima",
+      "Niki – Bima – Endra"
+    ],
+    kunci: 2
+  },
+  {
+    soal: "Tujuh orang, yaitu Aming, Bima, Cinta, Dewi, Eko, Fini, dan Ghea, melatih tujuh tim olahraga berbeda, yaitu tim A, B, C, D, E, F, dan G. Tim tersebut berlatih pada hari yang berbeda dari Senin sampai Minggu. Aming melatih tim D pada hari Rabu. Cinta tidak melatih tim A dan C. Jadwal latihannya sehari setelah Dewi melatih tim F. Fini melatih tim E pada hari Jumat. Ghea melatih sebuah tim pada hari Senin, tetapi bukan tim C maupun G. Jika Eko melatih tim A pada hari Kamis, kombinasi yang benar adalah ….",
+    opsi: [
+      "Dewi – F – Minggu",
+      "Bima – C – Sabtu",
+      "Cinta – G – Minggu",
+      "Ghea – G – Senin",
+      "Cinta – B – Sabtu"
+    ],
+    kunci: 2
+  },
+  {
+    soal: "Hasil operasi $0,25 + 1,14 - 0,73 + 0,56 \\times 2 = \\dots$",
+    opsi: [
+      "$1,68$",
+      "$1,78$",
+      "$1,21$",
+      "$2,44$",
+      "$2,64$"
+    ],
+    kunci: 1
+  },
+  {
+    soal: "Hasil operasi $3,5 + 1\\frac{7}{8} \\times \\frac{4}{3} \\div 2 + 0,25$ adalah ….",
+    opsi: [
+      "$3$",
+      "$5$",
+      "$8$",
+      "$10$",
+      "$15$"
+    ],
+    kunci: 1
+  },
+  {
+    soal: "Hasil operasi $0,875 \\times 2\\frac{2}{7} - 0,375 : 1\\frac{7}{8} = \\dots$",
+    opsi: [
+      "$2,55$",
+      "$1,89$",
+      "$1,80$",
+      "$1,55$",
+      "$0,89$"
+    ],
+    kunci: 2
+  },
+  {
+    soal: "Hasil operasi $0,82 \\times 1,16 - 0,16 \\times 0,84 + 16\\% - \\frac{3}{2} = \\dots$",
+    opsi: [
+      "$-0,5232$",
+      "$\\frac{1}{2}$",
+      "$0$",
+      "$-\\frac{1}{2}$",
+      "$-1$"
+    ],
+    kunci: 0
+  },
+  {
+    soal: "$1, -1, -2, 2, 0, 0, 4, 2, 3, \\dots, \\dots$",
+    opsi: [
+      "$4, -2$",
+      "$2, 5$",
+      "$7, 4$",
+      "$7, 5$",
+      "$4, -4$"
+    ],
+    kunci: 3
+  },
+  {
+    soal: "$-2, 1, 2, 5, 10, \\dots, \\dots$",
+    opsi: [
+      "$11, 26$",
+      "$13, 26$",
+      "$13, 14$",
+      "$18, 19$",
+      "$20, 40$"
+    ],
+    kunci: 1
+  },
+  {
+    soal: "$1, 3, -3, 9, 9, 27, \\dots, \\dots$",
+    opsi: [
+      "$-27, -81$",
+      "$27, -81$",
+      "$-27, 81$",
+      "$27, 81$",
+      "$81, -243$"
+    ],
+    kunci: 2
+  },
+  {
+    soal: "$2, 2, 3, 4, 4, \\dots, 5, 48, 6$",
+    opsi: [
+      "$5$",
+      "$6$",
+      "$10$",
+      "$12$",
+      "$20$"
+    ],
+    kunci: 3
+  },
+  {
+    soal: "Diketahui $A = -3$ dan $B = \\frac{1}{2}$<br><br><table border='1' cellpadding='8' cellspacing='0' style='border-collapse: collapse; text-align: center; margin: 10px 0; background: #fff;'><tr><th style='min-width: 120px;'>$X$</th><th style='min-width: 120px;'>$Y$</th></tr><tr><td>$6A - 5B$</td><td>$7A - 5B$</td></tr></table><br>Manakah hubungan yang paling tepat antara nilai $X$ dan $Y$ berdasarkan informasi di atas?",
+    opsi: [
+      "$X < Y$",
+      "$X > Y$",
+      "$X = Y$",
+      "$2X = 3Y$",
+      "$3Y = 2X$"
+    ],
+    kunci: 1
+  },
+  {
+    soal: "Diketahui tabel berikut.<br><br><table border='1' cellpadding='8' cellspacing='0' style='border-collapse: collapse; text-align: center; margin: 10px 0; background: #fff;'><tr><th style='min-width: 150px;'>$x$</th><th style='min-width: 150px;'>$y$</th></tr><tr><td>$14 \\times 56 - 14 \\times 35$</td><td>$14 - 56 \\times 35$</td></tr></table><br>Manakah hubungan $x$ dan $y$ berikut yang benar?",
+    opsi: [
+      "$x < y$",
+      "$x > y$",
+      "$x = y$",
+      "$x = 3y$",
+      "hubungan $x$ dan $y$ tidak dapat ditentukan"
+    ],
+    kunci: 1
+  },
+  {
+    soal: "Diketahui<br><br><table border='1' cellpadding='8' cellspacing='0' style='border-collapse: collapse; text-align: center; margin: 10px 0; background: #fff;'><tr><th style='min-width: 150px;'>$X$</th><th style='min-width: 150px;'>$Y$</th></tr><tr><td>$\\frac{1}{6} : \\frac{1}{2} \\times 125$</td><td>$12\\frac{1}{2} + 48 \\times 1\\frac{1}{2} - 3\\frac{1}{2}$</td></tr></table><br>Manakah hubungan yang benar antara kuantitas $X$ dan $Y$ berikut berdasarkan informasi yang diberikan?",
+    opsi: [
+      "$X > Y$",
+      "$3X < Y$",
+      "$2X < 3Y$",
+      "$3X = 2Y$",
+      "Hubungan $X$ dan $Y$ tidak dapat ditentukan"
+    ],
+    kunci: 2
+  },
+  {
+    soal: "Didit membeli 3 buku seharga Rp37.500,00.<br><br><table border='1' cellpadding='8' cellspacing='0' style='border-collapse: collapse; text-align: center; margin: 10px 0; background: #fff;'><tr><th style='min-width: 150px;'>$X$</th><th style='min-width: 150px;'>$Y$</th></tr><tr><td>Harga 2 buku dalam rupiah</td><td>Rp30.000,00</td></tr></table><br>Manakah hubungan yang tepat antara nilai $X$ dan $Y$ berdasarkan informasi yang diberikan?",
+    opsi: [
+      "$X < Y$",
+      "$X > Y$",
+      "$X = Y$",
+      "$X + Y = 0$",
+      "Hubungan $X$ dan $Y$ tidak dapat ditentukan"
+    ],
+    kunci: 0
+  },
+  {
+    soal: "Dalam suatu proses pengunduhan data, sebuah file sebesar 480 MB berhasil diunduh dalam rentang waktu tertentu dengan kecepatan internet konstan 20 MBps. Jika pada durasi waktu yang sama persis menggunakan layanan provider internet yang berbeda, ukuran file yang berhasil diunduh mencapai 720 MB. Kecepatan unduh konstan dari provider kedua tersebut adalah ....",
+    opsi: [
+      "25 MBps",
+      "28 MBps",
+      "30 MBps",
+      "32 MBps",
+      "35 MBps"
+    ],
+    kunci: 2
+  },
+  {
+    soal: "Kenzo mampu menyelesaikan target bacaan buku RPUL dalam waktu 15 jam dengan membaca konstan 20 halaman per jam. Jika buku yang sama dibaca oleh Ica dengan konstan 30 halaman per jam, waktu yang dibutuhkan Ica untuk menyelesaikan bacaan tersebut adalah ....",
+    opsi: [
+      "8 jam",
+      "9 jam",
+      "10 jam",
+      "11 jam",
+      "12 jam"
+    ],
+    kunci: 2
+  },
+  {
+    soal: "Sebuah proyek jalan dapat diselesaikan dalam waktu 12 bulan dengan mengerahkan 30 orang pekerja. Jika setengah pekerja mengundurkan diri setelah triwulan-kedua proyek berlangsung, maka berapa lama proyek itu akan tertunda?",
+    opsi: [
+      "2 bulan",
+      "4 bulan",
+      "6 bulan",
+      "8 bulan",
+      "12 bulan"
+    ],
+    kunci: 2
+  },
+  {
+    soal: "Dua orang laki-laki dapat menyelesaikan sebuah kerajinan dalam 6 jam. 2 orang wanita dapat mengerjakan kerajinan tersebut dalam 3 jam. Suatu hari, pelanggan memesan 3 buah kerajinan yang harus diselesaikan dalam 2 jam. Berapa orangkah pegawai perempuan yang dibutuhkan agar kerajinan tersebut selesai tepat waktu?",
+    opsi: [
+      "6",
+      "8",
+      "12",
+      "9",
+      "3"
+    ],
+    kunci: 3
+  },
+  {
+    soal: "<img src='gambar/no56_soal_paket1.png' style='max-width: 400px; width: 100%; height: auto;'>",
+    opsi: [
+      "<img src='gambar/no56_opsiA_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>",
+      "<img src='gambar/no56_opsiB_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>",
+      "<img src='gambar/no56_opsiC_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>",
+      "<img src='gambar/no56_opsiD_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>",
+      "<img src='gambar/no56_opsiE_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>"
+    ],
+    kunci: 0
+  },
+  {
+    soal: "<img src='gambar/no57_soal_paket1.png' style='max-width: 400px; width: 100%; height: auto;'>",
+    opsi: [
+      "<img src='gambar/no57_opsiA_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>",
+      "<img src='gambar/no57_opsiB_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>",
+      "<img src='gambar/no57_opsiC_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>",
+      "<img src='gambar/no57_opsiD_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>",
+      "<img src='gambar/no57_opsiE_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>"
+    ],
+    kunci: 1
+  },
+  {
+    soal: "<img src='gambar/no58_soal_paket1.png' style='max-width: 400px; width: 100%; height: auto;'>",
+    opsi: [
+      "<img src='gambar/no58_opsiA_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>",
+      "<img src='gambar/no58_opsiB_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>",
+      "<img src='gambar/no58_opsiC_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>",
+      "<img src='gambar/no58_opsiD_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>",
+      "<img src='gambar/no58_opsiE_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>"
+    ],
+    kunci: 2
+  },
+  {
+    soal: "",
+    opsi: [
+      "<img src='gambar/no59_opsiA_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>",
+      "<img src='gambar/no59_opsiB_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>",
+      "<img src='gambar/no59_opsiC_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>",
+      "<img src='gambar/no59_opsiD_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>",
+      "<img src='gambar/no59_opsiE_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>"
+    ],
+    kunci: 3
+  },
+  {
+    soal: "",
+    opsi: [
+      "<img src='gambar/no60_opsiA_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>",
+      "<img src='gambar/no60_opsiB_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>",
+      "<img src='gambar/no60_opsiC_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>",
+      "<img src='gambar/no60_opsiD_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>",
+      "<img src='gambar/no60_opsiE_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>"
+    ],
+    kunci: 4
+  },
+  {
+    soal: "",
+    opsi: [
+      "<img src='gambar/no61_opsiA_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>",
+      "<img src='gambar/no61_opsiB_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>",
+      "<img src='gambar/no61_opsiC_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>",
+      "<img src='gambar/no61_opsiD_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>",
+      "<img src='gambar/no61_opsiE_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>"
+    ],
+    kunci: 0
+  },
+  {
+    soal: "<img src='gambar/no62_soal_paket1.png' style='max-width: 400px; width: 100%; height: auto;'>",
+    opsi: [
+      "<img src='gambar/no62_opsiA_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>",
+      "<img src='gambar/no62_opsiB_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>",
+      "<img src='gambar/no62_opsiC_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>",
+      "<img src='gambar/no62_opsiD_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>",
+      "<img src='gambar/no62_opsiE_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>"
+    ],
+    kunci: 1
+  },
+  {
+    soal: "<img src='gambar/no63_soal_paket1.png' style='max-width: 400px; width: 100%; height: auto;'>",
+    opsi: [
+      "<img src='gambar/no63_opsiA_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>",
+      "<img src='gambar/no63_opsiB_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>",
+      "<img src='gambar/no63_opsiC_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>",
+      "<img src='gambar/no63_opsiD_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>",
+      "<img src='gambar/no63_opsiE_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>"
+    ],
+    kunci: 2
+  },
+  {
+    soal: "<img src='gambar/no64_soal_paket1.png' style='max-width: 400px; width: 100%; height: auto;'>",
+    opsi: [
+      "<img src='gambar/no64_opsiA_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>",
+      "<img src='gambar/no64_opsiB_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>",
+      "<img src='gambar/no64_opsiC_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>",
+      "<img src='gambar/no64_opsiD_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>",
+      "<img src='gambar/no64_opsiE_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>"
+    ],
+    kunci: 3
+  },
+  {
+    soal: "<img src='gambar/no65_soal_paket1.png' style='max-width: 400px; width: 100%; height: auto;'>",
+    opsi: [
+      "<img src='gambar/no65_opsiA_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>",
+      "<img src='gambar/no65_opsiB_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>",
+      "<img src='gambar/no65_opsiC_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>",
+      "<img src='gambar/no65_opsiD_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>",
+      "<img src='gambar/no65_opsiE_paket1.png' style='height: 80px; width: auto; vertical-align: middle;'>"
+    ],
+    kunci: 4
+  }
+];
