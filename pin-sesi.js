74be8cec9854d135e9cda7f5e4541599acc4312b
Ary <ketutarysujaya@gmail.com>
2026-09-05 11:44:01 +0800

    Update pin-sesi.js

--- pin-sesi.js
+++ pin-sesi.js
@@ -1,15 +1,21 @@
-// Fungsi untuk mengecek PIN dan mengembalikan paket soal yang sesuai
+function sandikanPin(pin) {
+  return btoa(pin).split('').reverse().join('');
+}
+
 function muatSoalBerdasarkanPin(pin) {
-  if (pin === "0001") {
-    // Paket Khusus 1
-    return [...soalTWK_1, ...soalTIU_1, ...soalTKP_1];
-  } 
-  else if (pin === "awlkejlkjweajwelkjaklejklajeawkaejlkjlekajlwajel") {
-    // Persiapan Paket Khusus 2
-    // return [...soalTWK_0002, ...soalTIU_0002, ...soalTKP_0002];
+  const pinRahasia = sandikanPin(pin);
+
+  if (pinRahasia === "==QMwADM") {
+    // Jika PIN 0001 diketik:
+    filePembahasan = 'pembahasan-1.pdf'; 
+    
+    // PASTIKAN: Nama variabel di bawah ini harus sama PERSIS 
+    // dengan nama variabel di dalam file soal-twk-1.js, soal-tiu-1.js, dst.
+    return [...soalTWK_1, ...soalTIU_1, ...soalTKP_1]; 
   } 
   else {
-    // Paket Utama / Publik (Jika PIN asal-asalan)
+    // Jika PIN Sembarang / Default diketik:
+    filePembahasan = 'pembahasan.pdf'; 
     return [...soalTWK, ...soalTIU, ...soalTKP];
   }
 }
