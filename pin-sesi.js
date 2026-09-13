02cb526cfbc6336f91f36a834d9bafd3c5b56b52
Ary <ketutarysujaya@gmail.com>
2026-09-13 08:51:28 +0800

    Update pin-sesi.js

--- pin-sesi.js
+++ pin-sesi.js
@@ -1,6 +1,4 @@
-// Fungsi rahasia untuk menyandikan teks PIN yang diketik peserta
 function sandikanPin(pin) {
-  // btoa() mengubah PIN menjadi Base64, lalu dibalik urutannya
   return btoa(pin).split('').reverse().join('');
 }
 
@@ -9,7 +7,7 @@ function muatSoalBerdasarkanPin(pin) {
   const pinRahasia = sandikanPin(pin);
 
   if (pinRahasia === "==QMwADM") {
-    // === JALUR PIN: 0001 (PAKET 1) ===
+    // === JALUR PIN: PAKET 1
     if (typeof soalTWK_1 === 'undefined' || typeof soalTIU_1 === 'undefined' || typeof soalTKP_1 === 'undefined') {
       alert("Sistem Macet: Variabel paket soal 1 tidak ditemukan! Pastikan Anda sudah mengubah nama variabel di DALAM file soal-twk-1.js, soal-tiu-1.js, dan soal-tkp-1.js menjadi soalTWK_1, soalTIU_1, dan soalTKP_1.");
       return [];
@@ -17,8 +15,8 @@ function muatSoalBerdasarkanPin(pin) {
     filePembahasan = 'pembahasan-1.pdf'; 
     return [...soalTWK_1, ...soalTIU_1, ...soalTKP_1]; 
   } 
-  else if (pinRahasia === "=DQ0ST1TMLET") {
-    // === JALUR PIN: LOLOSSKD (PAKET 2) ===
+  else if (pinRahasia === "=DQ0STN1TM9ET") { 
+    // === JALUR PIN: PAKET 2
     if (typeof soalTWK_2 === 'undefined' || typeof soalTIU_2 === 'undefined' || typeof soalTKP_2 === 'undefined') {
       alert("Sistem Macet: Variabel paket soal 2 tidak ditemukan! Pastikan Anda sudah mengubah nama variabel di DALAM file soal-twk-2.js, soal-tiu-2.js, dan soal-tkp-2.js menjadi soalTWK_2, soalTIU_2, dan soalTKP_2.");
       return [];
