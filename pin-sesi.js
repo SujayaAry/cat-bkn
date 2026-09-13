1bc4df64bbf7b89e29aa8309c783783683032013
Ary <ketutarysujaya@gmail.com>
2026-09-13 10:28:44 +0800

    Update pin-sesi.js

--- pin-sesi.js
+++ pin-sesi.js
@@ -1,33 +1,38 @@
+// Fungsi rahasia untuk menyandikan teks PIN yang diketik peserta
 function sandikanPin(pin) {
+  // btoa() mengubah PIN menjadi Base64, lalu dibalik urutannya
   return btoa(pin).split('').reverse().join('');
 }
 
 // Fungsi utama penentu paket soal dan file pembahasan
 function muatSoalBerdasarkanPin(pin) {
+  // .toUpperCase() otomatis membesarkan semua huruf agar kebal salah ketik
   const pinRahasia = sandikanPin(pin.toUpperCase());
 
   if (pinRahasia === "==QMwADM") {
-    // === JALUR PIN: PAKET 1 ===
+    // === JALUR PIN: 0001 (PAKET 1) ===
     if (typeof soalTWK_1 === 'undefined' || typeof soalTIU_1 === 'undefined' || typeof soalTKP_1 === 'undefined') {
-      alert("Sistem Macet: Variabel paket soal 1 tidak ditemukan! Pastikan Anda sudah mengubah nama variabel di DALAM file soal-twk-1.js, soal-tiu-1.js, dan soal-tkp-1.js menjadi soalTWK_1, soalTIU_1, dan soalTKP_1.");
+      alert("Sistem Macet: Variabel paket soal 1 tidak ditemukan! Pastikan nama variabel di dalam file JS Anda sudah benar.");
       return [];
     }
     filePembahasan = 'pembahasan-1.pdf'; 
     return [...soalTWK_1, ...soalTIU_1, ...soalTKP_1]; 
   } 
-  else if (pinRahasia === "==gNyAjM") { 
-    // === JALUR PIN: PAKET 2 ===
+  
+  else if (pinRahasia === "=Q0STN1TM9ET") { 
+    // === JALUR PIN: LOLOSSKD (PAKET 2) ===
     if (typeof soalTWK_2 === 'undefined' || typeof soalTIU_2 === 'undefined' || typeof soalTKP_2 === 'undefined') {
-      alert("Sistem Macet: Variabel paket soal 2 tidak ditemukan! Pastikan Anda sudah mendaftarkan soal-twk-2.js dkk di index.html dan menamai variabel di dalamnya dengan benar.");
+      alert("Sistem Macet: Variabel paket soal 2 tidak ditemukan! Pastikan Anda sudah mendaftarkan soal-twk-2.js dkk di index.html");
       return [];
     }
     filePembahasan = 'pembahasan-2.pdf'; 
     return [...soalTWK_2, ...soalTIU_2, ...soalTKP_2]; 
   }
+  
   else {
     // === JALUR DEFAULT / PUBLIK (PIN BEBAS) ===
     if (typeof soalTWK === 'undefined' || typeof soalTIU === 'undefined' || typeof soalTKP === 'undefined') {
-      alert("Sistem Macet: File paket utama (soal-twk.js dkk) tidak terbaca. Pastikan file tersebut sudah diunggah dan variabelnya bernama soalTWK, soalTIU, dan soalTKP.");
+      alert("Sistem Macet: File paket utama tidak terbaca.");
       return [];
     }
     filePembahasan = 'pembahasan.pdf'; 
