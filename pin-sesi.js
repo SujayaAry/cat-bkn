43492634c44c34d8e8d90a828bb5bd4f377cb5dc
Ary <ketutarysujaya@gmail.com>
2026-09-13 10:31:59 +0800

    Update pin-sesi.js

--- pin-sesi.js
+++ pin-sesi.js
@@ -1,16 +1,13 @@
-// Fungsi rahasia untuk menyandikan teks PIN yang diketik peserta
 function sandikanPin(pin) {
-  // btoa() mengubah PIN menjadi Base64, lalu dibalik urutannya
   return btoa(pin).split('').reverse().join('');
 }
 
 // Fungsi utama penentu paket soal dan file pembahasan
 function muatSoalBerdasarkanPin(pin) {
-  // .toUpperCase() otomatis membesarkan semua huruf agar kebal salah ketik
-  const pinRahasia = sandikanPin(pin.toUpperCase());
+  const pinRahasia = sandikanPin(pin);
 
   if (pinRahasia === "==QMwADM") {
-    // === JALUR PIN: 0001 (PAKET 1) ===
+    // === JALUR PIN: PAKET 1 ===
     if (typeof soalTWK_1 === 'undefined' || typeof soalTIU_1 === 'undefined' || typeof soalTKP_1 === 'undefined') {
       alert("Sistem Macet: Variabel paket soal 1 tidak ditemukan! Pastikan nama variabel di dalam file JS Anda sudah benar.");
       return [];
@@ -20,7 +17,7 @@ function muatSoalBerdasarkanPin(pin) {
   } 
   
   else if (pinRahasia === "=Q0STN1TM9ET") { 
-    // === JALUR PIN: LOLOSSKD (PAKET 2) ===
+    // === JALUR PIN: PAKET 2 ===
     if (typeof soalTWK_2 === 'undefined' || typeof soalTIU_2 === 'undefined' || typeof soalTKP_2 === 'undefined') {
       alert("Sistem Macet: Variabel paket soal 2 tidak ditemukan! Pastikan Anda sudah mendaftarkan soal-twk-2.js dkk di index.html");
       return [];
